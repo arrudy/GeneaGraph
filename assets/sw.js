2b0d607e8ffe05aebf4b5fcd79ac3e228dc15db5
arrudy <412268@student.agh.edu.pl>
2024-12-12 19:00:39 +0100

    Last fixes

--- assets/sw.js
+++ assets/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './ChmObl_front.js',
-  './ChmObl_front_bg.wasm',
+  './chm_obl_front.js',
+  './chm_obl_front_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
